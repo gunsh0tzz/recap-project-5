b999bedb52bc39cf01489b1020182b8dd90b58be
GuNShOtzZ <kevinloeding@outlook.de>
2023-12-15 15:06:14 +0100

    Add SWR config and data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data } = useSWR("https://example-apis.vercel.app/api/art");
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
